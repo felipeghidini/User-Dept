3a24c04e3f9e67919ca35738941c5c3aafffc7d4
felipeghidini <lipeghidini18@gmail.com>
2021-12-31 10:57:52 -0300

    Projeto iniciado

---
+++ userdept/src/test/java/com/devsuperior/userdeopt/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.userdeopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
